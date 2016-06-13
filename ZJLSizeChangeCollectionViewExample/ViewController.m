9af298693ed97665c6b74e19e47d3c357575788d
lovehuahua24 <lovehuahua24@gmail.com>
2016-06-13 12:33:09 -0400

    slider

--- ZJLSizeChangeCollectionViewExample/ViewController.m
+++ ZJLSizeChangeCollectionViewExample/ViewController.m
@@ -13,20 +13,29 @@
 #define ScreenHeight [UIScreen mainScreen].bounds.size.height
 @interface ViewController ()<UICollectionViewDelegate,UICollectionViewDataSource>
 @property (nonatomic, copy) NSArray *data;
+@property (nonatomic, strong) UICollectionView *collectionView;
+@property (nonatomic, strong) ZJLCollectionViewFlowLayout *layout;
+@property (nonatomic, strong) UISlider *slider;
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    ZJLCollectionViewFlowLayout *layout = [[ZJLCollectionViewFlowLayout alloc]init];
-    layout.itemSize = CGSizeMake(150, 150);
-    UICollectionView *collectionView = [[UICollectionView alloc] initWithFrame:CGRectMake(0, 0, ScreenWidth, ScreenHeight) collectionViewLayout:layout];
-    collectionView.delegate = self;
-    collectionView.dataSource = self;
-    collectionView.backgroundColor = [UIColor whiteColor];
-    [self.view addSubview:collectionView];
-    [collectionView registerNib:[UINib nibWithNibName:@"ZJLCollectionViewCell" bundle:nil] forCellWithReuseIdentifier:@"my_cell"];
+    self.layout = [[ZJLCollectionViewFlowLayout alloc]init];
+    self.layout.itemSize = CGSizeMake(150, 150);
+    self.collectionView = [[UICollectionView alloc] initWithFrame:CGRectMake(0, 100, ScreenWidth, ScreenHeight-100) collectionViewLayout:self.layout];
+    self.collectionView.delegate = self;
+    self.collectionView.dataSource = self;
+    self.collectionView.backgroundColor = [UIColor whiteColor];
+    [self.view addSubview:self.collectionView];
+    [self.collectionView registerNib:[UINib nibWithNibName:@"ZJLCollectionViewCell" bundle:nil] forCellWithReuseIdentifier:@"my_cell"];
+    self.slider = [[UISlider alloc] initWithFrame:CGRectMake(0, (ScreenWidth-200)/2, 200, 50)];
+    self.slider.minimumValue = 0;
+    self.slider.maximumValue = (self.collectionView.bounds.size.height-self.layout.itemSize.height)/2;
+    self.slider.value = 0;
+    [self.slider addTarget:self action:@selector(sliderValueChanged:) forControlEvents:UIControlEventValueChanged];
+    [self.view addSubview:self.slider];
     self.data = [NSArray arrayWithObjects:@"Montreal",@"Toronto",@"Ottawa",@"Calgary",@"Vancouver",@"Quebec city", nil];
 }
 
@@ -41,6 +50,18 @@
     cell.titleLabel.text = self.data[indexPath.row];
     cell.backgroundColor = [self randomColor];
     return cell;
+}
+
+- (UIEdgeInsets)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout*)collectionViewLayout insetForSectionAtIndex:(NSInteger)section{
+    CGFloat insetGap = (self.collectionView.frame.size.width - self.layout.itemSize.width) * 0.5;
+    return UIEdgeInsetsMake(self.slider.value, insetGap, self.slider.value, insetGap);
+}
+
+- (void)sliderValueChanged:(id)sender
+{
+    CGFloat insetGap = (self.collectionView.frame.size.width - self.layout.itemSize.width) * 0.5;
+    self.layout.sectionInset = UIEdgeInsetsMake(self.slider.value, insetGap, self.slider.value, insetGap);
+    [self.collectionView reloadData];
 }
 
 - (UIColor *)randomColor
